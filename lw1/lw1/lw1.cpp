7234e234e6b8b4b2dd311f982271407fedf5ef3d
whoami <whoami@DESKTOP-60MC21L>
2023-09-15 00:11:15 +0300

    Добавьте файлы проекта.

---
+++ lw1/lw1/lw1.cpp
@@ -0,0 +1,28 @@
+﻿#include <windows.h>
+#include <string>
+#include <iostream>
+
+DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
+{
+	std::cout << lpParam << std::endl;
+	ExitThread(0);
+}
+
+int main()
+{
+	// создание двух потоков
+	HANDLE* handles = new HANDLE[2];
+
+	handles[0] = CreateThread(NULL, 0, &ThreadProc, NULL, CREATE_SUSPENDED,
+		NULL);
+	handles[1] = CreateThread(NULL, 0, &ThreadProc, NULL, CREATE_SUSPENDED,
+		NULL);
+
+
+
+	ResumeThread(handles[0]);
+	ResumeThread(handles[1]);
+
+	WaitForMultipleObjects(2, handles, true, INFINITE);
+	return 0;
+}

5394b5f312ecc6d3696945d554462a39404f38db
rakuzzan <marishka1581@gmail.com>
2023-12-01 02:26:21 +0300

    lw7

---
+++ lab7.2/lab7.2/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        const string apiUrl = "https://dog.ceo/api/breeds/image/random";
+        const int numberOfRequests = 10;
+
+        Console.WriteLine("Асинхронная загрузка:");
+
+        await DownloadImagesAsync(apiUrl, numberOfRequests);
+
+        Console.WriteLine("\nСинхронная загрузка:");
+
+        DownloadImagesSync(apiUrl, numberOfRequests);
+    }
+
+    static async Task DownloadImagesAsync(string apiUrl, int numberOfRequests)
+    {
+        var httpClient = new HttpClient();
+
+        var downloadTasks = new Task[numberOfRequests];
+
+        for (int i = 0; i < numberOfRequests; i++)
+        {
+            downloadTasks[i] = DownloadImageAsync(apiUrl, httpClient);
+        }
+
+        await Task.WhenAll(downloadTasks);
+    }
+
+    static void DownloadImagesSync(string apiUrl, int numberOfRequests)
+    {
+        var httpClient = new HttpClient();
+
+        for (int i = 0; i < numberOfRequests; i++)
+        {
+            DownloadImageSync(apiUrl, httpClient).Wait();
+        }
+    }
+
+    static async Task DownloadImageAsync(string apiUrl, HttpClient httpClient)
+    {
+        try
+        {
+            HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+            response.EnsureSuccessStatusCode();
+
+            string content = await response.Content.ReadAsStringAsync();
+            string imageUrl = ParseJson(content);
+
+            Console.WriteLine($"Starting download from URL: {imageUrl}");
+
+            await DownloadAndSaveImageAsync(imageUrl);
+
+            Console.WriteLine($"Image downloaded successfully: {imageUrl}");
+
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Error downloading image from {apiUrl}: {ex.Message}");
+        }
+    }
+
+    static async Task DownloadImageSync(string apiUrl, HttpClient httpClient)
+    {
+        try
+        {
+            HttpResponseMessage response = httpClient.GetAsync(apiUrl).Result;
+
+            response.EnsureSuccessStatusCode();
+
+            string content = await response.Content.ReadAsStringAsync();
+            string imageUrl = ParseJson(content);
+
+            Console.WriteLine($"Starting download from URL: {imageUrl}");
+
+            DownloadAndSaveImageSync(imageUrl);
+
+            Console.WriteLine($"Image downloaded successfully: {imageUrl}"); 
+        }
+        catch (HttpRequestException ex)
+        {
+            Console.WriteLine($"Error downloading image from {apiUrl}: {ex.Message}");
+        }
+    }
+
+    static string ParseJson(string jsonString)
+    {
+        using (JsonDocument document = JsonDocument.Parse(jsonString))
+        {
+            return document.RootElement.GetProperty("message").GetString();
+        }
+    }
+
+    static async Task DownloadAndSaveImageAsync(string imageUrl)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            byte[] imageBytes = await client.GetByteArrayAsync(imageUrl);
+            string fileName = Path.GetFileName(imageUrl);
+            await File.WriteAllBytesAsync(fileName, imageBytes);
+        }
+    }
+
+    static void DownloadAndSaveImageSync(string imageUrl)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            byte[] imageBytes = client.GetByteArrayAsync(imageUrl).Result;
+            string fileName = Path.GetFileName(imageUrl);
+            File.WriteAllBytes(fileName, imageBytes);
+        }
+    }
+}
---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к текстовому файлу:");
+        string filePath = Console.ReadLine();
+
+        try
+        {
+            string text = await ReadTextFromFileAsync(filePath);
+
+            Console.WriteLine("Введите список символов для удаления (без пробелов):");
+            string charactersToRemove = Console.ReadLine();
+
+            text = RemoveCharacters(text, charactersToRemove);
+
+            await SaveTextToFileAsync(filePath, text);
+
+            Console.WriteLine("Операция завершена успешно.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}");
+        }
+    }
+
+    static async Task<string> ReadTextFromFileAsync(string filePath)
+    {
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            return await reader.ReadToEndAsync();
+        }
+    }
+
+    static string RemoveCharacters(string text, string charactersToRemove)
+    {
+        foreach (char c in charactersToRemove)
+        {
+            text = text.Replace(c.ToString(), string.Empty);
+        }
+
+        return text;
+    }
+
+    static async Task SaveTextToFileAsync(string filePath, string text)
+    {
+        using (StreamWriter writer = new StreamWriter(filePath, false, Encoding.UTF8))
+        {
+            await writer.WriteAsync(text);
+        }
+    }
+}
